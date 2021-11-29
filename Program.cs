feda40d036550dade2dca12ce4129b97a360bfbc
Mosgovskii <aleksejgit44@gmail.com>
2021-11-29 15:56:46 +0200

    main

--- Program.cs
+++ Program.cs
@@ -10,6 +10,38 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
+            double num1 = 0, num2 = 0;
+            var ordinary = new Ordinary();
+            var advanced = new Advanced();
+            while (true)
+            {
+                Console.WriteLine("1 - Ordinary calc\n2 - Advanced calc\nOther to exit");
+                var r = Console.ReadLine();
+                if (r == "1")
+                {
+                    try
+                    {
+                        Console.WriteLine("Choose the action (+,-,*,/)");
+                        var a = Console.ReadLine();
+                        Console.WriteLine("Enter the first number");
+                        num1 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Enter the second number");
+                        num2 = Convert.ToDouble(Console.ReadLine());
+                        if (a == "+") Console.WriteLine($"Answer: {ordinary.Add(num1, num2)}");
+                        else if (a == "-") Console.WriteLine($"Answer: {ordinary.Sub(num1, num2)}");
+                        else if (a == "*") Console.WriteLine($"Answer: {ordinary.Mul(num1, num2)}");
+                        else if (a == "/")
+                        {
+                            if (num2 == 0) throw new Exception();
+                            Console.WriteLine($"Answer: {ordinary.Div(num1, num2)}");
+                        }
+                        else Console.WriteLine("The action was chosen incorrectly");
+                    }
+                    catch { Console.WriteLine("Numbers was entred incorrectly"); }
+                }
+                
+                else break;
+            }
         }
     }
 }
